36143f6e00a482002d20f515371d2f712a767895
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2021-03-22 21:34:22 +0100

    coded CV + data generation + bibliography

---
+++ code/cross_validation.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+def mtl_cross_val(estimator, criterion, X, Y, n_folds=5):
+    """Carries out a cross validation to estimate the performance
+       of an multi-task LASSO estimator.
+
+    ..note-to-self: In an inverse problem in neuroscience, partitioning the
+    X into folds consists in partitioning with respect to the sensors on the 
+    scalp. This is why CV makes less sense in this kind of inverse problem than
+    on vanilla prediction problems. In vanilla prediction problems, samples in X 
+    are expected to be i.i.d., while in an inverse problem like this one X represents
+    the geometry of the brain and data fails to be i.i.d.
+
+    As a reminder: 
+        n_samples: number of sensors on the scalp
+        n_features: number of areas in the brain
+        n_tasks: time series length
+
+    Args:
+        estimator (BaseEstimator): sklearn estimator
+        criterion (callable): CV metric
+        X (np.ndarray): Design matrix (n_samples, n_features)
+        Y (np.ndarray): Target matrix (n_samples, n_tasks)
+        n_folds (int, optional): [description]. Defaults to 5.
+    """
+    Y_oof = np.zeros_like(Y)
+    n_samples = X.shape[0]
+
+    folds = np.array_split(range(n_samples), 5)
+
+    for i in range(len(folds)):
+        train_indices = np.concatenate([fold for j, fold in enumerate(folds) if i != j])
+        valid_indices = folds[i]
+
+        X_train, Y_train = X[train_indices, :], Y[train_indices, :]
+        X_valid, Y_valid = X[valid_indices, :], Y[valid_indices, :]
+
+        estimator.fit(X_train, Y_train)
+        Y_pred = estimator.predict(X_valid)
+
+        Y_oof[valid_indices, :] = Y_pred
+    
+    return criterion(Y, Y_oof)
+
+    
+
+        
--- code/simulated_data.py
+++ code/simulated_data.py
@@ -1,13 +1,19 @@
 import numpy as np
 
-def simulate_data(n_samples, n_features, n_tasks, nnz, snr):
-    """Generates a simulated row-sparse weight 
-       matrix for multi-task lasso
+def simulate_data(n_samples=100, n_features=10000, n_tasks=100, nnz=10, snr=3,
+                  mean_X=.0, std_X=1.5, mean_W=.0, std_W=.5):
+    """Generates a simulated dataset and a row-sparse weight 
+       matrix for multi-task lasso.
+
+       As a reminder (in neuroscience inverse problem):
+        n_samples: number of sensors on the scalp
+        n_features: number of discretized areas in the brain 
+        n_tasks: time series length of the brain recording
 
     Args:
         n_samples (int): number of samples
         n_features (int): number of features
-        n_tasks (int): [description]
+        n_tasks (int): number of tasks
         nnz (int): number of non-zero coefficients
         snr (float): signal-to-noise ratio
 
@@ -17,4 +23,25 @@ def simulate_data(n_samples, n_features, n_tasks, nnz, snr):
         W (np.ndarray): (n_features, n_tasks) 
     """
 
-    return
+    if nnz > n_features:
+        raise ValueError("Number of non-zero coefficients can't be greater than the number of features")
+
+    X = np.random.normal(loc=mean_X, scale=std_X, size=(n_samples, n_features))
+
+    dense_W = np.random.normal(loc=mean_W, scale=std_W, size=(n_features, n_tasks))
+    sparse_mat = np.zeros_like(dense_W)
+
+    for i in range(n_tasks):
+        col = np.array([1] * nnz + [0] * (n_features - nnz)).T
+        np.random.shuffle(col)
+        sparse_mat[:, i] = col
+    
+    W = dense_W * sparse_mat
+    Y = X @ W
+
+    # Adding a pre-defined SNR to a signal: https://sites.ualberta.ca/~msacchi/SNR_Def.pdf
+    noise = np.random.normal(size=(n_samples, n_tasks))
+    alpha = np.sqrt((np.linalg.norm(Y, axis=0) ** 2) / (snr * np.linalg.norm(noise, axis=0) ** 2))
+    Y += alpha * noise
+
+    return X, Y, W

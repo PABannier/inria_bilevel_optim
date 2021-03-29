fedbaf28c0b3b6d62083f23dfefa1adc6336e390
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2021-03-29 16:30:02 +0200

    ENH comparison plot reweighted non reweighted visualization

--- mtl_code/examples/plot_mse_path.py
+++ mtl_code/examples/plot_mse_path.py
@@ -121,6 +121,110 @@ def plot_comparison_mse_path_lasso():
     plt.show(block=True)
 
 
+def plot_comparison_mse_path_lasso_across_folds():
+    X, Y, _ = simulate_data(
+        n_samples=50,
+        n_features=250,
+        n_tasks=25,
+        nnz=10,
+        corr=0.2,
+        random_state=2020,
+        snr=1,
+    )
+
+    X = np.asfortranarray(X)
+    Y = np.asfortranarray(Y)
+
+    n_folds = 5
+
+    alpha_max = compute_alpha_max(X, Y)
+    print("Alpha max for large experiment:", alpha_max)
+
+    alphas = np.geomspace(alpha_max / 100, alpha_max, num=100)
+
+    # Multi-task LASSO
+    mtl_lasso = MultiTaskLassoCV(cv=n_folds, random_state=2020, alphas=alphas)
+    mtl_lasso.fit(X, Y)
+
+    # Reweighted multi-task LASSO
+    reweighted_mtl_lasso = ReweightedMultiTaskLassoCV(alphas, n_folds=n_folds)
+    reweighted_mtl_lasso.fit(X, Y)
+
+    fig, axes = plt.subplots(1, 2, figsize=(14, 7))
+
+    for idx_fold in range(n_folds):
+        axes[0].semilogx(
+            alphas,
+            mtl_lasso.mse_path_[:, idx_fold],
+            linestyle="--",
+            label=f"Fold {idx_fold + 1}",
+        )
+
+    axes[0].semilogx(
+        alphas,
+        mtl_lasso.mse_path_.mean(axis=1),
+        linewidth=2,
+        color="black",
+        label="Mean",
+    )
+
+    mtl_min_idx = mtl_lasso.mse_path_.mean(axis=1).argmin()
+    axes[0].axvline(
+        x=alphas[mtl_min_idx],
+        color="black",
+        linestyle="dashed",
+        linewidth=3,
+        label="Best alpha",
+    )
+
+    for idx_fold in range(n_folds):
+        axes[1].semilogx(
+            alphas,
+            reweighted_mtl_lasso.mse_path_[:, idx_fold],
+            linestyle="--",
+            label=f"Fold {idx_fold + 1}",
+        )
+
+    axes[1].semilogx(
+        alphas,
+        reweighted_mtl_lasso.mse_path_.mean(axis=1),
+        linewidth=2,
+        color="black",
+        label="Mean",
+    )
+
+    reweighted_mtl_min_idx = reweighted_mtl_lasso.mse_path_.mean(axis=1).argmin()
+    axes[1].axvline(
+        x=alphas[reweighted_mtl_min_idx],
+        color="black",
+        linestyle="dashed",
+        linewidth=3,
+        label="Best alpha",
+    )
+
+    axes[0].set_xlabel("alpha", fontsize=12)
+    axes[1].set_xlabel("alpha", fontsize=12)
+    axes[0].set_ylabel("MSE", fontsize=12)
+
+    axes[0].set_title(
+        "Non-reweighted MTL",
+        fontsize=15,
+        fontweight="bold",
+    )
+
+    axes[1].set_title(
+        "Reweighted MTL",
+        fontsize=15,
+        fontweight="bold",
+    )
+
+    fig.suptitle("MSE path", fontsize=20, fontweight="bold")
+
+    axes[0].legend(loc=1)
+    axes[1].legend(loc=1)
+    plt.show(block=True)
+
+
 def plot_mse_path_reweighted_mtl_wrt_correlation():
     correlation_coefficients = [0.1, 0.3, 0.5, 0.7, 0.9]
     colors = ["r", "b", "g", "y", "black"]
@@ -225,6 +329,7 @@ def plot_mse_path_wrt_num_iterations(corr=0.2):
 
 if __name__ == "__main__":
     # plot_mse_path_reweighted_mtl()
-    plot_mse_path_wrt_num_iterations(corr=0.7)
+    # plot_mse_path_wrt_num_iterations(corr=0.7)
     # plot_comparison_mse_path_lasso()
+    plot_comparison_mse_path_lasso_across_folds()
     # plot_mse_path_reweighted_mtl_wrt_correlation()

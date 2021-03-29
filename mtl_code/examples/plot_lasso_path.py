31b9975df08d5c7544737e24d9a87b90fef1fb6a
QB3 <quentin.bertrand@inria.fr>
2021-03-29 10:03:28 +0200

    added example lasso path

---
+++ mtl_code/examples/plot_lasso_path.py
@@ -0,0 +1,33 @@
+import numpy as np
+from numpy.linalg import norm
+
+import matplotlib.pyplot as plt
+
+from mtl.cross_validation import MultiTaskLassoCV
+from mtl.simulated_data import simulate_data
+from celer.plot_utils import configure_plt
+
+configure_plt()
+
+X, Y, _ = simulate_data(
+    n_samples=50, n_features=250, n_tasks=25, nnz=10, corr=0.2,
+    random_state=2020, snr=3
+)
+
+alpha_max = np.max(norm(X.T @ Y, axis=1)) / X.shape[0]
+print("Alpha max for large experiment:", alpha_max)
+
+n_folds = 5
+alphas = np.geomspace(alpha_max, alpha_max / 100, num=30)
+regressor_cv = MultiTaskLassoCV(alphas, n_folds=n_folds)
+regressor_cv.fit(X, Y)
+
+
+plt.figure(figsize=(8, 6))
+for idx_fold in range(n_folds):
+    plt.semilogx(alphas, regressor.mse_path_[:, idx_fold], linestyle="--")
+plt.semilogx(alphas, regressor.mse_path_.mean(axis=1), linewidth=4, color="black")
+plt.xlabel("alpha", fontsize=12)
+plt.xticks(np.arange(len(alphas)), xlabels)
+plt.ylabel("MSE", fontsize=12)
+plt.show(block=True)

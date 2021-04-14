85188313cfd80ebba931719d61e18e2ab2db3bad
Pierre-Antoine Bannier <pierre-antoine@macbook-pro-de-pierre-antoine.home>
2021-04-14 16:00:46 +0200

    ENH Plot for tex

--- mtl_code/examples/mne_solver/plot_for_tex_2.py
+++ mtl_code/examples/mne_solver/plot_for_tex_2.py
@@ -1,3 +1,5 @@
+import argparse
+
 import joblib
 from itertools import cycle
 
@@ -13,6 +15,26 @@ from mpl_toolkits.axes_grid1 import (
 
 import mne
 from mne.viz.utils import _get_color_list
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--estimator",
+    help="choice of estimator to reconstruct the channels. available: "
+    + "lasso-cv, lasso-sure, adaptive-cv, adaptive-sure",
+)
+parser.add_argument(
+    "--condition",
+    help="choice of condition. available: "
+    + "Left Auditory, Right Auditory, Left visual, Right visual",
+)
+
+args = parser.parse_args()
+
+if args.estimator is None or args.condition is None:
+    raise ValueError("You need to choose an estimator and a condition.")
+else:
+    ESTIMATOR = args.estimator
+    CONDITION = args.condition
 
 
 def plot_source_activations(src, stcs, axes, labels=None, linewidth=2):
@@ -61,23 +83,34 @@ def plot_source_activations(src, stcs, axes, labels=None, linewidth=2):
                 linestyle=linestyle,
             )
 
-    # axes.set_aspect(1.1)
-
     for tick in axes.yaxis.get_major_ticks():
         tick.label.set_fontsize(8)
 
     for tick in axes.xaxis.get_major_ticks():
         tick.label.set_fontsize(8)
 
-    axes.set_xlabel("Time (ms)", fontsize=8)
-    axes.set_ylabel("Source amplitude (nAm)", fontsize=8)
+    axes.set_xlabel("Time (ms)", fontsize=8, fontweight="bold")
+    axes.set_ylabel("Source amplitude (nAm)", fontsize=8, fontweight="bold")
 
     return fig
 
 
+def add_foci_to_brain_surface(brain, hemi, color):
+    i = 0 if hemi == "lh" else 1
+
+    try:
+        activation_idx = stc.vertices[i][0]
+        surface_coords = brain.geo[hemi].coords
+
+        foci_coords = surface_coords[activation_idx]
+
+        brain.add_foci(foci_coords, hemi=hemi, color=color)  # Color???
+    except IndexError:
+        print(f"Could not find an activation for hemisphere {hemi}")
+
+
 data_path = mne.datasets.sample.data_path()
 subjects_dir = op.join(data_path, "subjects")
-fname_stc = op.join(data_path, "MEG", "sample", "sample_audvis-meg-eeg-lh.stc")
 fname_evoked = op.join(data_path, "MEG", "sample", "sample_audvis-ave.fif")
 fname_fwd = data_path + "/MEG/sample/sample_audvis-meg-eeg-oct-6-fwd.fif"
 
@@ -87,13 +120,17 @@ evoked.pick_types(meg="grad").apply_baseline((None, 0.0))
 max_t = evoked.get_peak()[1]
 
 forward = mne.read_forward_solution(fname_fwd)
-stc = joblib.load("left_auditory/data/stc_adaptive-sure.pkl")
 
-colormap = "viridis"
+folder_name = CONDITION.lower().replace(" ", "_")
+stc = joblib.load(f"{folder_name}/data/stc_{ESTIMATOR}.pkl")
+
+colormap = "inferno"
+
+view = "lateral" if "auditory" in CONDITION else "caudal"
 
 # Plot the STC, get the brain image, crop it:
 brain = stc.plot(
-    views="lat",
+    views=view,
     hemi="split",
     size=(1000, 500),
     subject="sample",
@@ -106,6 +143,13 @@ brain = stc.plot(
     time_viewer=False,
     show_traces=False,
 )
+
+brain.set_time(0.1)
+
+# Adding foci
+add_foci_to_brain_surface(brain, "lh", "blue")
+add_foci_to_brain_surface(brain, "rh", "red")
+
 screenshot = brain.screenshot()
 brain.close()
 
@@ -125,14 +169,6 @@ plt.rcParams.update(
     }
 )
 
-# figsize unit is inches
-# fig, axes = plt.subplots(
-#    nrows=2,
-#    ncols=1,
-#    figsize=(4.5, 3.0),
-# gridspec_kw=dict(height_ratios=[4, 4]),
-# )
-
 fig = plt.figure(figsize=(4.5, 3.0))
 axes = [
     plt.subplot2grid((7, 1), (0, 0), rowspan=4),
@@ -142,7 +178,6 @@ axes = [
 evoked_idx = 1
 brain_idx = 0
 
-
 plot_source_activations(forward["src"], stc, axes=axes[evoked_idx])
 
 # now add the brain to the lower axes
@@ -151,9 +186,10 @@ axes[brain_idx].axis("off")
 
 # add a vertical colorbar with the same properties as the 3D one
 divider = make_axes_locatable(axes[brain_idx])
-cax = divider.append_axes("right", size="5%", pad=0.2)
+# cax = divider.append_axes("right", size="5%", pad=0.2)
+cax = divider.append_axes("bottom", size="15%", pad=0.2)
 cbar = mne.viz.plot_brain_colorbar(
-    cax, "auto", colormap, orientation="vertical"  # label="Activation (F)",
+    cax, "auto", colormap, orientation="horizontal"  # label="Activation (F)",
 )
 
 # tweak margins and spacing
@@ -161,19 +197,11 @@ fig.subplots_adjust(
     left=0.15, right=0.9, bottom=0.15, top=0.9, wspace=0.1, hspace=0.2
 )
 
-# add subplot labels
-for ax, label in zip(axes, "AB"):
-    ax.text(
-        0.03,
-        ax.get_position().ymax,
-        label,
-        transform=fig.transFigure,
-        fontsize=12,
-        fontweight="bold",
-        va="top",
-        ha="left",
-    )
+fig_dir = (
+    "../../../tex/article/srcimages/blobs/"
+    + f"blob-{folder_name}-{ESTIMATOR}.png"
+)
 
-fig.savefig("ex2.svg")
+fig.savefig(fig_dir)
 
-plt.show(block=True)
+plt.show()

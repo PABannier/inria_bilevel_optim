2f2dbe5f2ce760a55903e1a794d9ef872857787d
Pierre-Antoine Bannier <pierre-antoine@macbook-pro-de-pierre-antoine.home>
2021-04-19 20:38:16 +0200

    ENH Blob plot for tex

--- mtl_code/examples/mne_solver/plot_for_tex_2.py
+++ mtl_code/examples/mne_solver/plot_for_tex_2.py
@@ -13,8 +13,21 @@ from mpl_toolkits.axes_grid1 import (
     inset_locator,
 )
 
+from numba import njit
+
 import mne
 from mne.viz.utils import _get_color_list
+
+plt.rcParams.update(
+    {
+        "ytick.labelsize": "small",
+        "xtick.labelsize": "small",
+        "axes.labelsize": "small",
+        "axes.titlesize": "medium",
+        "grid.color": "0.75",
+        "grid.linestyle": ":",
+    }
+)
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -37,78 +50,19 @@ else:
     CONDITION = args.condition
 
 
-def plot_source_activations(src, stcs, axes, labels=None, linewidth=2):
-    if not isinstance(stcs, list):
-        stcs = [stcs]
-    if labels is not None and not isinstance(labels, list):
-        labels = [labels]
-
-    colors = _get_color_list()
-    linestyles = ["-", "--", ":"]
-
-    lh_points = src[0]["rr"]
-
-    vertnos = [
-        np.r_[stc.lh_vertno, lh_points.shape[0] + stc.rh_vertno]
-        for stc in stcs
-    ]
-    unique_vertnos = np.unique(np.concatenate(vertnos).ravel())
-
-    colors = cycle(colors)
-
-    if labels is not None:
-        colors = [
-            next(colors)
-            for _ in range(np.unique(np.concatenate(labels).ravel()).size)
-        ]
-
-    for idx, v in enumerate(unique_vertnos):
-        ind = [k for k, vertno in enumerate(vertnos) if v in vertno]
-
-        if labels is None:
-            c = next(colors)
-        else:
-            c = colors[labels[ind[0]][vertnos[ind[0]] == v]]
-
-        for k in ind:
-            vertno = vertnos[k]
-            mask = vertno == v
-            assert np.sum(mask) == 1
-            linestyle = linestyles[k]
-            axes.plot(
-                1e3 * stcs[k].times,
-                1e9 * stcs[k].data[mask].ravel(),
-                c=c,
-                linewidth=linewidth,
-                linestyle=linestyle,
-            )
-
-    for tick in axes.yaxis.get_major_ticks():
-        tick.label.set_fontsize(8)
-
-    for tick in axes.xaxis.get_major_ticks():
-        tick.label.set_fontsize(8)
-
-    axes.set_xlabel("Time (ms)", fontsize=8)
-    axes.set_ylabel("Source amplitude (nAm)", fontsize=8)
-
-    return fig
-
-
-def add_foci_to_brain_surface(brain, hemi, color):
-    i = 0 if hemi == "lh" else 1
-
-    try:
-        activation_idx = stc.vertices[i][0]
+def add_foci_to_brain_surface(brain, stc, ax):
+    for i_hemi, hemi in enumerate(["lh", "rh"]):
         surface_coords = brain.geo[hemi].coords
+        hemi_data = stc.lh_data if hemi == "lh" else stc.rh_data
+        for k in range(len(stc.vertices[i_hemi])):
+            activation_idx = stc.vertices[i_hemi][k]
+            foci_coords = surface_coords[activation_idx]
 
-        foci_coords = surface_coords[activation_idx]
+            (line,) = ax.plot(stc.times, 1e9 * hemi_data[k])
+            brain.add_foci(foci_coords, hemi=hemi, color=line.get_color())
 
-        brain.add_foci(foci_coords, hemi=hemi, color=color)  # Color???
-    except IndexError:
-        print(f"Could not find an activation for hemisphere {hemi}")
-    except TypeError:
-        print(f"Could not find an activation for hemisphere {hemi}")
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Amplitude (nAm)")
 
 
 data_path = mne.datasets.sample.data_path()
@@ -129,8 +83,6 @@ stc = joblib.load(f"{folder_name}/data/stc_{ESTIMATOR}.pkl")
 lower_bound = round(stc.data.min() * 1e9)
 upper_bound = round(stc.data.max() * 1e9)
 
-print(lower_bound)
-print(upper_bound)
 
 colormap = "inferno"
 clim = dict(
@@ -138,11 +90,10 @@ clim = dict(
     lims=(lower_bound, 0, upper_bound),
 )
 
-view = "lateral" if "auditory" in CONDITION.lower() else "caudal"
 
 # Plot the STC, get the brain image, crop it:
 brain = stc.plot(
-    views=view,
+    views=["lat", "med"],
     hemi="split",
     size=(1000, 500),
     subject="sample",
@@ -154,32 +105,12 @@ brain = stc.plot(
     colormap=colormap,
     time_viewer=False,
     show_traces=False,
+    cortex="bone",
+    volume_options=dict(resolution=1),
 )
 
-brain.set_time(0.1)
+brain.set_time(0.05)
 
-# Adding foci
-add_foci_to_brain_surface(brain, "lh", "blue")
-add_foci_to_brain_surface(brain, "rh", "red")
-
-screenshot = brain.screenshot()
-brain.close()
-
-nonwhite_pix = (screenshot != 255).any(-1)
-nonwhite_row = nonwhite_pix.any(1)
-nonwhite_col = nonwhite_pix.any(0)
-cropped_screenshot = screenshot[nonwhite_row]  # [:, nonwhite_col]
-
-plt.rcParams.update(
-    {
-        "ytick.labelsize": "small",
-        "xtick.labelsize": "small",
-        "axes.labelsize": "small",
-        "axes.titlesize": "medium",
-        "grid.color": "0.75",
-        "grid.linestyle": ":",
-    }
-)
 
 fig = plt.figure(figsize=(4.5, 4.5))
 axes = [
@@ -187,26 +118,37 @@ axes = [
     plt.subplot2grid((7, 1), (4, 0), rowspan=3),
 ]
 
+add_foci_to_brain_surface(brain, stc, axes[1])
+
+screenshot = brain.screenshot()
+brain.close()
+
+nonwhite_pix = (screenshot != 255).any(-1)
+nonwhite_row = nonwhite_pix.any(1)
+nonwhite_col = nonwhite_pix.any(0)
+
+
+@njit
+def add_margin(nonwhite_col, margin=5):
+    margin_nonwhite_col = nonwhite_col.copy()
+    for i in range(len(nonwhite_col)):
+        if nonwhite_col[i] == True and nonwhite_col[i - 1] == False:
+            margin_nonwhite_col[i - (1 + margin) : i - 1] = True
+        elif nonwhite_col[i] == False and nonwhite_col[i - 1] == True:
+            margin_nonwhite_col[i : i + margin] = True
+    return margin_nonwhite_col
+
+
+# Add blank columns for margin
+nonwhite_col = add_margin(nonwhite_col)
+
+cropped_screenshot = screenshot[nonwhite_row][:, nonwhite_col]
+
 evoked_idx = 1
 brain_idx = 0
 
-plot_source_activations(forward["src"], stc, axes=axes[evoked_idx])
-
-# now add the brain to the lower axes
 axes[brain_idx].imshow(cropped_screenshot)
 axes[brain_idx].axis("off")
-
-# add a vertical colorbar with the same properties as the 3D one
-divider = make_axes_locatable(axes[brain_idx])
-# cax = divider.append_axes("right", size="5%", pad=0.2)
-cax = divider.append_axes("bottom", size="15%", pad=0.2)
-cbar = mne.viz.plot_brain_colorbar(
-    cax,
-    clim,
-    colormap,
-    orientation="horizontal",
-    label="",
-)
 
 # tweak margins and spacing
 fig.subplots_adjust(

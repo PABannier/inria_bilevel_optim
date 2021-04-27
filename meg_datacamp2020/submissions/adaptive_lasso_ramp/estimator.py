465a2a18fa6c012ab51fc009aab285d285f5c342
Pierre-Antoine Bannier <pbannier@drago3.saclay.inria.fr>
2021-04-27 16:55:26 +0200

    CLN lasso solver

---
+++ meg_datacamp2020/submissions/adaptive_lasso_ramp/estimator.py
@@ -0,0 +1,267 @@
+import os
+from joblib import parallel_backend
+from joblib import Parallel, delayed
+from tqdm import tqdm
+import time
+
+import numpy as np
+from numpy.linalg import norm
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.utils import check_X_y, check_array
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils import check_random_state
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.base import TransformerMixin, RegressorMixin
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import jaccard_score
+from sklearn.linear_model import LassoLars
+
+
+N_JOBS = 20  # -1
+INNER_MAX_NUM_THREADS = 1
+
+MEMMAP_FOLDER = "."
+OUTPUT_FILENAME_MEMMAP = os.path.join(MEMMAP_FOLDER, "output_memmap")
+
+
+def compute_alpha_max(X, y):
+    return np.max(np.abs(X.T @ y)) / len(X)
+
+
+def _get_coef(est):
+    """Get coefficients from a fitted regression estimator."""
+    if hasattr(est, "steps"):
+        return est.steps[-1][1].coef_
+    return est.coef_
+
+
+class SparseRegressor(BaseEstimator, ClassifierMixin, TransformerMixin):
+    """Provided regression estimator (ie model) solves inverse problem
+    using data X and lead field L. The estimated coefficients (est_coef
+    sometimes called z) are then used to predict which parcels are active.
+
+    X must be of a specific structure with a column name 'subject' and
+    'L_path' which gives the path to lead_field files for each subject
+    """
+
+    def __init__(self, model, n_jobs=1):
+        self.model = model
+        self.n_jobs = n_jobs
+        self.parcel_indices = {}
+        self.Ls = {}
+
+    def fit(self, X, y):
+        return self
+
+    def predict(self, X):
+        return (self.decision_function(X) > 0).astype(int)
+
+    def _run_model(self, model, L, X):
+        norms = np.linalg.norm(L, axis=0)
+        L = L / norms[None, :]
+
+        est_coefs = np.memmap(
+            OUTPUT_FILENAME_MEMMAP,
+            dtype=np.float32,
+            shape=(X.shape[0], L.shape[1]),
+            mode="w+",
+        )
+
+        start_time = time.time()
+
+        with parallel_backend(
+            "loky", inner_max_num_threads=INNER_MAX_NUM_THREADS
+        ):
+            Parallel(N_JOBS)(
+                delayed(self._fit_model_for_sample)(
+                    L, X, idx, model, est_coefs
+                )
+                for idx in range(len(X))
+            )
+
+        print("Fitting duration:", time.time() - start_time)
+
+        return est_coefs.T
+
+    def _fit_model_for_sample(self, L, X, idx, model, est_coefs):
+        if idx % 10 == 0:
+            print(f"Fitting idx #{idx}")
+        x = X.iloc[idx].values
+        model.fit(L, x)
+        est_coef = np.abs(_get_coef(model))
+        est_coefs[idx] = est_coef
+
+    def decision_function(self, X):
+        X = X.reset_index(drop=True)
+
+        for subject_id in np.unique(X["subject"]):
+            if subject_id not in self.Ls:
+                # load corresponding L if it's not already in
+                L_used = X[X["subject"] == subject_id]["L_path"].iloc[0]
+                lead_field = np.load(L_used)
+                self.parcel_indices[subject_id] = lead_field["parcel_indices"]
+
+                # scale L to avoid tiny numbers
+                self.Ls[subject_id] = 1e8 * lead_field["lead_field"]
+                assert (
+                    self.parcel_indices[subject_id].shape[0]
+                    == self.Ls[subject_id].shape[1]
+                )
+
+        n_parcels = np.max([np.max(s) for s in self.parcel_indices.values()])
+        betas = np.empty((len(X), n_parcels))
+        for subj_idx in np.unique(X["subject"]):
+            L_used = self.Ls[subj_idx]
+
+            X_used = X[X["subject"] == subj_idx]
+            X_used = X_used.drop(["subject", "L_path"], axis=1)
+
+            est_coef = self._run_model(self.model, L_used, X_used)
+
+            beta = (
+                pd.DataFrame(np.abs(est_coef))
+                .groupby(self.parcel_indices[subj_idx])
+                .max()
+                .transpose()
+            )
+            betas[X["subject"] == subj_idx] = np.array(beta)
+        return betas
+
+
+class CustomSparseEstimator(BaseEstimator, RegressorMixin):
+    """Regression estimator which uses LassoLars algorithm with given alpha
+    normalized for each lead field L and x."""
+
+    def __init__(self):
+        self.best_sure_ = np.inf
+        self.best_alpha_ = None
+        self.coef_ = None
+
+    def fit(self, L, x):
+        L = StandardScaler().fit_transform(L)
+        self.best_sure_ = np.inf
+        self.best_alpha_ = None
+        self.coef_ = None
+
+        sure_path_ = []
+
+        alpha_max = compute_alpha_max(L, x)
+        alphas = np.geomspace(alpha_max, alpha_max / 500, 15)
+
+        # Sigma = 1 confirmé???????
+
+        for alpha in alphas:
+            estimator = SURE(SingleTaskReweightedLASSO, 1, random_state=0)
+
+            sure_val_ = estimator.get_val(L, x, alpha)
+            sure_path_.append(sure_val_)
+
+            if sure_val_ < self.best_sure_:
+                self.best_sure_ = sure_val_
+                self.best_alpha_ = alpha
+            else:
+                diffs = np.diff(sure_path_)
+                if np.all(diffs[-3:] >= 0):
+                    print("Early stopping.")
+                    break
+
+        # Refitting
+        estimator = SingleTaskReweightedLASSO(self.best_alpha_)
+        estimator.fit(L, x)
+
+        self.coef_ = estimator.coef_
+
+
+def get_estimator():
+    custom_model = CustomSparseEstimator()
+    adaptive_lasso = SparseRegressor(custom_model)
+
+    return adaptive_lasso
+
+
+# ====================================================================
+# ============================= UTILS ================================
+# ====================================================================
+
+
+class SingleTaskReweightedLASSO(BaseEstimator, RegressorMixin):
+    def __init__(self, alpha=0.1, n_iterations=10, warm_start=True):
+        self.alpha = alpha
+        self.n_iterations = n_iterations
+        self.warm_start = warm_start
+
+        self.coef_ = None
+
+        self.regressor = LassoLars(
+            alpha=alpha, fit_intercept=False, normalize=False
+        )
+
+        self.penalty = lambda w: 1.0 / (
+            2.0 * np.sqrt(np.abs(w)) + np.finfo(float).eps
+        )
+
+    def fit(self, X, y):
+        X, y = check_X_y(X, y)
+        n_samples, n_features = X.shape
+
+        w = np.ones(n_features)
+
+        for k in range(self.n_iterations):
+            X_w = X / w[np.newaxis, :]
+            self.regressor.fit(X_w, y)
+            coef_ = self.regressor.coef_ / w
+
+            w = self.penalty(coef_)
+
+        self.coef_ = coef_
+
+    def predict(self, X):
+        check_is_fitted(self)
+        X = check_array(X)
+        return X @ self.coef_
+
+
+class SURE:
+    def __init__(self, estimator, sigma, random_state=None):
+        self.estimator = estimator
+        self.sigma = sigma
+        self.rng = check_random_state(random_state)
+
+        self.eps = None
+        self.delta = None
+
+    def get_val(self, X, Y, alpha, n_iterations=5, **estimator_kwargs):
+        n_samples = Y.shape[0]
+
+        if self.delta is None or self.eps is None:
+            self.init_eps_and_delta(n_samples)
+
+        # fit 2 models in Y and Y + epsilon * delta
+        model = self.estimator(alpha, n_iterations, **estimator_kwargs)
+        model.fit(X, Y)
+        coef1 = model.coef_
+        model.fit(X, Y + self.eps * self.delta)
+        coef2 = model.coef_
+
+        # Note: Celer returns the transpose of the coefficient
+        # matrix
+        if coef1.shape[0] != X.shape[1]:
+            coef1 = coef1.T
+            coef2 = coef2.T
+
+        # compute the dof
+        dof = ((X @ coef2 - X @ coef1) * self.delta).sum() / self.eps
+        # compute the SURE
+        sure = norm(Y - X @ coef1) ** 2
+        sure -= n_samples * self.sigma ** 2
+        sure += 2 * dof * self.sigma ** 2
+
+        return sure
+
+    def init_eps_and_delta(self, n_samples):
+        self.eps = 2 * self.sigma / (n_samples ** 0.3)
+        self.delta = self.rng.randn(n_samples)
--- solver_code/solver_lasso/solver_free_orient.py
+++ solver_code/solver_lasso/solver_free_orient.py
@@ -1,16 +1,23 @@
 import numpy as np
 from numpy.linalg import norm
 
+from numba import njit
+
 from sklearn.utils import check_random_state
 from mtl.utils_datasets import compute_alpha_max
 
-import ipdb
+from celer import MultiTaskLasso
+
+# import ipdb
 
 
 # Utils functions
 
 
 def sum_squared(X):
+    """Computes the Frobenius norm for a matrix
+    in a more efficient way.
+    """
     X_flat = X.ravel(order="F" if np.isfortran(X) else "C")
     return np.dot(X_flat, X_flat)
 
@@ -35,17 +42,37 @@ def norm_l_2_inf(A, n_orient):
     return np.sqrt(np.max(group_norm_2(A, n_orient)))
 
 
+def get_duality_gap(X, coef, Y, alpha, n_orient):
+    Y_hat = X @ coef
+    R = Y - Y_hat
+
+    # Primal
+    penalty = norm_l_2_1(coef, n_orient)
+    nR2 = sum_squared(R)
+    p_obj = 0.5 * nR2 + alpha * penalty
+
+    # Dual
+    dual_norm = norm_l_2_inf(X.T @ R, n_orient)
+    scaling = alpha / dual_norm
+    scaling = min(scaling, 1.0)
+
+    d_obj = (scaling - 0.5 * (scaling ** 2)) * nR2 + scaling * np.sum(
+        R * Y_hat
+    )
+
+    gap = p_obj - d_obj
+    return gap, p_obj, d_obj
+
+
 def block_soft_thresh(Y, alpha, n_orient):
     """Proximal operator for l_2_1 norm, also called
     block soft thresholding.
     """
+
     n_positions = Y.shape[0] // n_orient
-    # rows_norm = np.sqrt(group_norm_2(Y, n_orient))
     rows_norm = np.sqrt(
         (Y * Y.conj()).real.reshape(n_positions, -1).sum(axis=1)
     )
-
-    # rows_norm = np.sqrt(sum_squared(Y))
 
     shrink = np.maximum(1.0 - alpha / np.maximum(rows_norm, alpha), 0.0)
 
@@ -60,94 +87,60 @@ def block_soft_thresh(Y, alpha, n_orient):
     return Y
 
 
-def get_duality_gap(X, coef, Y, alpha, n_orient):
-    Y_hat = X @ coef
-    R = Y - Y_hat
-
-    penalty = norm_l_2_1(coef, n_orient)
-
-    # Why not Frobenius norm?
-    # R_flat = R.ravel(order="F" if np.isfortran(R) else "C")
-    # nR2 = np.dot(R_flat, R_flat)
-    nR2 = sum_squared(R)
-    p_obj = 0.5 * nR2 + alpha * penalty
-
-    # Dual
-    dual_norm = norm_l_2_inf(X.T @ R, n_orient)
-    scaling = alpha / dual_norm
-    scaling = min(scaling, 1.0)
-
-    d_obj = (scaling - 0.5 * (scaling ** 2)) * norm(R) ** 2 + scaling * np.sum(
-        R * Y_hat
-    )
-
-    gap = p_obj - d_obj
-    return gap, p_obj, d_obj
-
-
-def _bcd(X, coef, Y, list_X_j_c, lipschitz_consts, n_orient):
+def _bcd(X, coef, Y, list_X_j_c, R, lipschitz_consts, n_orient, alpha):
     """Implements one full pass of BCD.
     BCD stands for Block Coordinate Descent.
     """
-    R = Y - X @ coef
-    coef_j_new = np.zeros_like(X[0:n_orient, :], order="C")
-
     for j, X_j_c in enumerate(list_X_j_c):
         idx = slice(j * n_orient, (j + 1) * n_orient)
-        X_j = X[:, idx]
-        coef_j = coef[idx, :]
+        coef_j = coef[idx].copy()
 
-        coef_j_new = X_j.T @ R  # / lipschitz_consts[j]
+        coef_j_new = X_j_c.T @ R / -lipschitz_consts[j]
         coef_j_new += coef_j
 
         coef[idx, :] = block_soft_thresh(
             coef_j_new, alpha / lipschitz_consts[j], n_orient
         )
 
-        # alpha_lc = alpha / lipschitz_consts[j]
-
-        # coef[idx, :] = block_soft_thresh(coef_j_new, alpha_lc, n_orient)
-
-    return coef
+        R += X_j_c @ (coef[idx, :] - coef_j)
 
 
 if __name__ == "__main__":
-    rng = check_random_state(0)
+    rng = check_random_state(42)
 
     X = rng.randn(10, 15)
     Y = rng.randn(10, 5)
 
-    X = np.asfortranarray(X)
-
-    MAX_ITER = 100
-    CHECK_DGAP_FREQ = 5
+    MAX_ITER = 20
+    CHECK_DGAP_FREQ = 1
     TOL = 1e-8
 
     n_samples, n_features = X.shape
     _, n_tasks = Y.shape
-    n_orient = 1
+    n_orient = 3
 
     n_positions = n_features // n_orient
 
     alpha_max = compute_alpha_max(X, Y)
     print("Alpha max:", alpha_max)
-
     alpha = alpha_max * 0.1
 
     coef_ = np.zeros((n_features, n_tasks))
 
-    lipschitz_consts = (X ** 2).sum(axis=0)
-
     # Storing list of contiguous arrays
     list_X_j_c = []
+    lipschitz_consts = np.empty((n_positions))
+
+    R = Y.copy()
+
     for j in range(n_positions):
         idx = slice(j * n_orient, (j + 1) * n_orient)
         list_X_j_c.append(np.ascontiguousarray(X[:, idx]))
+        lipschitz_consts[j] = norm(X[:, idx].T @ X[:, idx])
 
     for iter_idx in range(MAX_ITER):
         # ipdb.set_trace()
-
-        coef_ = _bcd(X, coef_, Y, list_X_j_c, lipschitz_consts, n_orient)
+        _bcd(X, coef_, Y, list_X_j_c, R, lipschitz_consts, n_orient, alpha)
 
         if iter_idx % CHECK_DGAP_FREQ == 0:
             gap, p_obj, d_obj = get_duality_gap(X, coef_, Y, alpha, n_orient)
@@ -158,3 +151,6 @@ if __name__ == "__main__":
             if gap < TOL:
                 print("Threshold reached. Stopped fitting.")
                 break
+
+    estimator = MultiTaskLasso(alpha, verbose=1, fit_intercept=False)
+    estimator.fit(X, Y)

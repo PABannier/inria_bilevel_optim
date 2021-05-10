FIX Alpha scaling in free orientation solver

mtl_code/examples/mne_examples/free_orientation/generate_reports.py
@@ -1,5 +1,6 @@
 import argparse
 import joblib
+import time
 from pathlib import Path
 from tqdm import tqdm
 import os.path as op
@@ -249,10 +250,6 @@ def solver(M, G, n_orient=1):
     print("Alpha max:", alpha_max)
 
     alphas = np.geomspace(alpha_max, alpha_max / 10, num=15)
-    if n_orient == 3:
-        alphas = len(G) * alphas  # Unscaled
-
-    import time
 
     start = time.time()
 

mtl_code/examples/mne_examples/free_orientation/manual_experiment.py
@@ -1,5 +1,6 @@
 import argparse
 import joblib
+import time
 from tqdm import tqdm
 import os.path as op
 
@@ -126,22 +127,17 @@ def solver(M, G, n_orient=1):
         )
         estimator.fit(G, M)
     else:
-        alpha_max = compute_alpha_max(G, M, n_orient=n_orient)  # Scaled
+        alpha_max = compute_alpha_max(G, M, n_orient=n_orient)
         print("Alpha max:", alpha_max)
 
         alphas = np.geomspace(alpha_max, alpha_max / 10, num=15)
-
-        if n_orient == 3:
-            alphas = len(G) * alphas  # Unscaled
-
-        import time
 
         start = time.time()
 
         criterion = SUREForReweightedMultiTaskLasso(
             1, alphas, n_orient=n_orient
         )
-        best_sure, best_alpha = criterion.get_val(G, M)  # Unscaled
+        best_sure, best_alpha = criterion.get_val(G, M)
 
         print("Duration:", time.time() - start)
 
@@ -149,9 +145,7 @@ def solver(M, G, n_orient=1):
         print("Best alpha:", best_alpha)
 
         # Refitting
-        estimator = ReweightedMultiTaskLasso(
-            best_alpha, n_orient=n_orient
-        )  # Unscaled
+        estimator = ReweightedMultiTaskLasso(best_alpha, n_orient=n_orient)
         estimator.fit(G, M)
 
     X = estimator.coef_

mtl_code/mtl/solver_free_orient.py
@@ -14,7 +14,7 @@ from mtl.utils_datasets import (
 )
 
 
-class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
+class MultiTaskLassoUnscaled(BaseEstimator, RegressorMixin):
     """MultiTask Lasso solver specificially designed for
     neuroscience inverse problem. It supports fixed and free
     dipole orientations.
@@ -56,13 +56,6 @@ class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
     active_set_size: int, default=50
         Size of active set increase at each iteration.
 
-    normalized_alpha: bool, default=True
-        This solver expects an unscaled alpha (no normalization
-        by the number of samples or tasks). If True, this solver
-        expects a scaled alpha and will unscale it on its own. If
-        False, this solver expects an unscaled alpha and won't
-        change it.
-
     verbose: bool, default=False
         Verbosity.
 
@@ -85,7 +78,6 @@ class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
         accelerated=True,
         K=5,
         active_set_size=100,
-        normalized_alpha=True,
         verbose=False,
     ):
         self.alpha = alpha
@@ -96,7 +88,6 @@ class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
         self.accelerated = accelerated
         self.K = K
         self.active_set_size = active_set_size
-        self.normalized_alpha = normalized_alpha
         self.verbose = verbose
 
         self.gap_history_ = []
@@ -104,7 +95,7 @@ class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
         self.coef_ = None
         self.active_set_ = None
 
-    def fit(self, X, Y):
+    def _fit(self, X, Y, alpha=None):
         """Fits the MultiTaskLasso estimator to the X and Y
         matrices.
 
@@ -118,12 +109,16 @@ class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
 
         Y: array of shape (n_samples, n_times)
             Target matrix.
-        """
-        X, Y = check_X_y(X, Y, multi_output=True)
 
-        # # If needed, unscale alpha by the number of samples
-        # if self.normalized_alpha:
-        #     self.alpha *= len(X)
+        alpha: float, default=None
+            Regularization parameter. If None, the solver uses self.alpha.
+            This parameter is useful when calling the fit method in
+            MultiTaskLassoOrientation.
+        """
+
+        _alpha = alpha if alpha is not None else self.alpha
+
+        X, Y = check_X_y(X, Y, multi_output=True)
 
         n_samples, n_features = X.shape
         n_times = Y.shape[1]
@@ -173,14 +168,14 @@ class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
             ]
 
             coef, as_ = self._block_coordinate_descent(
-                X[:, active_set], Y, lipschitz_consts_tmp, coef_init
+                X[:, active_set], Y, lipschitz_consts_tmp, coef_init, _alpha
             )
 
             active_set[active_set] = as_.copy()
             idx_old_active_set = np.where(active_set)[0]
 
             _, p_obj, d_obj = get_duality_gap_mtl(
-                X, Y, coef, active_set, self.alpha, self.n_orient
+                X, Y, coef, active_set, _alpha, self.n_orient
             )
 
             highest_d_obj = max(highest_d_obj, d_obj)
@@ -250,7 +245,7 @@ class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
         X = check_array(X)
         return X @ self.coef_
 
-    def _block_coordinate_descent(self, X, Y, lipschitz, init):
+    def _block_coordinate_descent(self, X, Y, lipschitz, init, _alpha):
         """Implements a block coordinate descent algorithm using
         Anderson acceleration.
 
@@ -268,6 +263,11 @@ class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
         init: array (shape varies with active set)
             Coefficient initialized from previous iteration.
             If None, coefficients are initialized with zeros.
+
+        _alpha: float, default=None
+            Regularization parameter. If None, the solver uses self.alpha.
+            This parameter is useful when calling the fit method in
+            MultiTaskLassoOrientation.
 
         Returns
         -------
@@ -330,7 +330,7 @@ class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
                     coef_j_new += coef_j
 
                 block_norm = np.sqrt(sum_squared(coef_j_new))
-                alpha_lc = self.alpha / lipschitz[j]
+                alpha_lc = _alpha / lipschitz[j]
 
                 if block_norm <= alpha_lc:
                     coef_j.fill(0.0)
@@ -352,7 +352,7 @@ class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
                     active_set[idx] = True
 
             _, p_obj, d_obj = get_duality_gap_mtl(
-                X, Y, coef[active_set], active_set, self.alpha, self.n_orient
+                X, Y, coef[active_set], active_set, _alpha, self.n_orient
             )
             highest_d_obj = max(d_obj, highest_d_obj)
             gap = p_obj - highest_d_obj
@@ -391,7 +391,7 @@ class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
                             Y,
                             coef_acc[active_set_acc],
                             active_set_acc,
-                            self.alpha,
+                            _alpha,
                             self.n_orient,
                         )
 
@@ -411,3 +411,13 @@ class MultiTaskLassoOrientation(BaseEstimator, RegressorMixin):
 
         coef = coef[active_set]
         return coef, active_set
+
+
+class MultiTaskLassoOrientation(MultiTaskLassoUnscaled):
+    def __init__(self, alpha, **kwargs):
+        super().__init__(alpha, **kwargs)
+
+    def fit(self, X, Y):
+        alpha_scaled = self.alpha * len(X)
+        self._fit(X, Y, alpha_scaled)
+        return self

mtl_code/mtl/tests/test_mtl_free_orient.py
@@ -14,8 +14,11 @@ from celer import Lasso
 
 from mtl.simulated_data import simulate_data
 
-from mtl.solver_free_orient import MultiTaskLassoOrientation
-from mtl.utils_datasets import norm_l2_inf
+from mtl.solver_free_orient import (
+    MultiTaskLassoUnscaled,
+    MultiTaskLassoOrientation,
+)
+from mtl.utils_datasets import norm_l2_inf, compute_alpha_max
 
 ####################
 # Mathematical tests
@@ -45,11 +48,11 @@ def test_gap_decreasing(
     alpha_max = norm_l2_inf(np.dot(X.T, Y), n_orient, copy=False)
     alpha = alpha_max * alpha_frac
 
-    estimator = MultiTaskLassoOrientation(
+    estimator = MultiTaskLassoUnscaled(
         alpha, n_orient, accelerated=accelerated
     )
 
-    estimator.fit(X, Y)
+    estimator._fit(X, Y)
 
     XR = X.T @ (Y - X @ estimator.coef_)
     active_set = norm(estimator.coef_, axis=1) != 0
@@ -76,11 +79,11 @@ def test_sparsity(n_orient, accelerated):
     alpha_max = norm_l2_inf(np.dot(X.T, Y), n_orient, copy=False)
     alpha = alpha_max * 0.1
 
-    estimator = MultiTaskLassoOrientation(
+    estimator = MultiTaskLassoUnscaled(
         alpha, n_orient, accelerated=accelerated
     )
 
-    estimator.fit(X, Y)
+    estimator._fit(X, Y)
 
     assert np.sum(norm(estimator.coef_, axis=1) == 0) > 5
 
@@ -107,12 +110,12 @@ def test_single_task_fixed_orient(
     alpha_max = np.max(np.abs(X.T @ y))
     alpha = alpha_max * alpha_frac
 
-    estimator1 = MultiTaskLassoOrientation(
+    estimator1 = MultiTaskLassoUnscaled(
         alpha, 1, accelerated=accelerated, tol=1e-12
     )
     estimator2 = Lasso(alpha / len(X), fit_intercept=False, tol=1e-12)
 
-    estimator1.fit(X, y)
+    estimator1._fit(X, y)
     estimator2.fit(X, y)
 
     np.testing.assert_allclose(
@@ -138,7 +141,7 @@ def test_multi_task_fixed_orient(
     alpha_max = norm_l2_inf(X.T @ Y, n_orient, copy=False)
     alpha = alpha_max * alpha_frac
 
-    estimator = MultiTaskLassoOrientation(
+    estimator = MultiTaskLassoUnscaled(
         alpha,
         n_orient,
         accelerated=accelerated,
@@ -147,7 +150,7 @@ def test_multi_task_fixed_orient(
         max_iter=10000,
     )
 
-    estimator.fit(X, Y)
+    estimator._fit(X, Y)
 
     coef, active_set, gap_history = mixed_norm_solver(
         Y, X, alpha, n_orient=n_orient, debias=False, tol=1e-12, maxit=10000
@@ -158,13 +161,3 @@ def test_multi_task_fixed_orient(
         final_coef_[active_set] = coef
 
     np.testing.assert_allclose(estimator.coef_, final_coef_)
-
-
-if __name__ == "__main__":
-
-    for (n_samples, n_features), n_orient, accelerated, alpha_frac in product(
-        DATA_SIZE, N_ORIENTS, ACCELERATED, ALPHA_FRAC
-    ):
-        test_multi_task_fixed_orient(
-            n_samples, n_features, n_orient, accelerated, alpha_frac
-        )

mtl_code/mtl/tests/test_mtl_reweighted.py
@@ -15,7 +15,7 @@ from mtl.utils_datasets import compute_alpha_max
 ####################
 
 
-N_ORIENTS = [3]  # 1
+N_ORIENTS = [1, 3]
 DATA_SIZE = [(10, 15), (50, 75)]
 ALPHA_FRAC = [0.5, 0.1, 0.05, 0.01]
 
@@ -32,7 +32,7 @@ def test_training_loss_decrease(n_orient, n_samples, n_features, alpha_frac):
         random_state=0,
     )
 
-    alpha_max = compute_alpha_max(X, Y)
+    alpha_max = compute_alpha_max(X, Y, n_orient)
     alpha = alpha_max * alpha_frac
 
     regressor = ReweightedMultiTaskLasso(
@@ -46,15 +46,3 @@ def test_training_loss_decrease(n_orient, n_samples, n_features, alpha_frac):
     diffs = np.diff(regressor.loss_history_)
     np.testing.assert_array_less(diffs, 1e-5)
     assert start_loss > final_loss
-
-
-if __name__ == "__main__":
-
-    for n_orient, (n_samples, n_features), alpha_frac in product(
-        [3], DATA_SIZE, ALPHA_FRAC
-    ):
-        print(n_samples, n_features)
-        print(alpha_frac)
-        test_training_loss_decrease(
-            n_orient, n_samples, n_features, alpha_frac
-        )
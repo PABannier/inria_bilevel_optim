77f08a1a20326b04f4374f61047acad11367de63
QB3 <quentin.bertrand@inria.fr>
2022-05-09 11:46:51 -0400

    made implicit diff work for dof

--- mtl_code/mtl/lasso_sure.py
+++ mtl_code/mtl/lasso_sure.py
@@ -11,25 +11,26 @@ n_tasks = 1
 X, y = simulate_data(n_samples, n_features, n_tasks, nnz=3, random_state=0)[:2]
 y = np.ravel(y)
 alpha_max = norm(X.T @ y, ord=np.inf) / len(X)
-alpha = alpha_max * 0.5
+alpha = alpha_max * 0.1
 
 L = norm(X.T @ X, ord=2)
 
 # Closed-form solution: dof = support size
 clf = Lasso(alpha, fit_intercept=False)
 clf.fit(X, y)
-dof_cf = np.sum(clf.coef_ != 0)
+supp = clf.coef_ != 0
+dof_cf = np.sum(supp != 0)
 
 # Analytical solution via implicit differentiation
-beta = clf.coef_
-z = beta - (1 / L) * X.T @ (X @ beta - y)
-jacob_prox_z = np.zeros_like(z)
-jacob_prox_z[np.abs(z) >= alpha] = 1
-jacob_prox_z = np.diag(jacob_prox_z)
+# beta = clf.coef_
+# z = beta - (1 / L) * X.T @ (X @ beta - y)
+# supp = np.abs(z) >= alpha
+jacob_prox_z = np.ones(supp.sum())  # to be modified for other penalties
 
-id_feat = np.eye(n_features)
-J = np.linalg.inv(id_feat - jacob_prox_z @ (id_feat - X.T @ X / L)) @ jacob_prox_z @ X.T / L
-dof_id = np.trace(X @ J)
+id_feat = np.eye(supp.sum())
+X_supp = X[:, supp]
+J = np.linalg.inv(id_feat - jacob_prox_z * (id_feat - X_supp.T @ X_supp / L)) @ (jacob_prox_z[:, None] * X_supp.T / L)
+dof_id = np.trace(X_supp @ J)
 
 print("DOF")
 print("Closed form:", dof_cf)
